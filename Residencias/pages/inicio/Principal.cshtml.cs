c10c8353cd5d93eb954ebf6e7416f4afd5eb674c
Adriana2521 <191G0265@rcarbonifera.tecnm.mx>
2023-12-04 21:09:07 -0600

    Agregar archivos de proyecto.

---
+++ Residencias/pages/inicio/Principal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Residencias.pages.inicio
+{
+    public class PrincipalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Residencias/pages/inicio/Promociones.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Residencias.pages.inicio
+{
+    public class PromocionesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
